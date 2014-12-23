5ef5533e3e4b5405f57741d29ec34599fb208dea
ManjunathChet <ManjunathChet@gmail.com>
2014-12-22 19:45:43 -0800

    Modified the TMPFILE so it is created in global /tmp/ instead of a local directory (SQUASH)

--- http.sh
+++ http.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# FIXME: what if the file named "pipe" already exists and is being used by something else?
-mkfifo pipe
+#TMPFILE is a unique filename
+TMPFILE=$(mktemp -ut tmp_pipe.XXXXXXX) || exit 1
+
+#make a pipe with this unique name
+mkfifo "$TMPFILE"
+
 doBreak=0
 
 #trap SIGINT and cause it to set doBreak to 1
 trap "doBreak=1" SIGINT
 
 while true; do
-	nc -l 8080 < pipe | ./http-stdin.sh > pipe
+	nc -l 8080 < $TMPFILE | ./http-stdin.sh > $TMPFILE
 
 	#if SIGINT has been received, break the loop
 	if [ "$doBreak" -eq 1 ]; then
+		#delete the temp file
+		rm "$TMPFILE"
 		break
 	fi
 done
 
-
 # FIXME: every other web request has a broken connection;
 # to get the points, you'll have to make every web request succeed
-
-
-
